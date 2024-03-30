d66edc86a8920be70c4d92e7384248135b2df52e
Jakub Jarosz <jakub@jarosz.dev>
2024-03-30 07:01:19 +0000

    Use test helper instead of log

--- pypi_test.go
+++ pypi_test.go
@@ -3,7 +3,6 @@ package gopypi_test
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -20,10 +19,12 @@ func readFile(filepath string) ([]byte, error) {
 	return b, nil
 }
 
-func mockServer() *httptest.Server {
+func testServer(t *testing.T) *httptest.Server {
+	t.Helper()
+
 	content, err := readFile("testdata/response_pytest_json.json")
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 
 	f := func(w http.ResponseWriter, r *http.Request) {
@@ -31,40 +32,41 @@ func mockServer() *httptest.Server {
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintln(w, string(content))
 	}
-
 	return httptest.NewServer(http.HandlerFunc(f))
 }
 
 func TestPackageService_Get(t *testing.T) {
-	server := mockServer()
+	t.Parallel()
+
+	server := testServer(t)
 	defer server.Close()
 
 	client := gopypi.NewClient()
 	client.BaseURL = server.URL
 
-	arg := "pytest"
-	got, err := client.Get(context.Background(), arg)
+	pythonPackage := "pytest"
+	got, err := client.Get(context.Background(), pythonPackage)
 	if err != nil {
-		t.Errorf("client.Package.Get(%q) = %v", arg, got)
+		t.Errorf("client.Package.Get(%q) = %v", pythonPackage, got)
 	}
 
 	want := "pytest"
 	if got.Info.Name != want {
-		t.Errorf("client.Package.Get(%q) = %s; want %s", arg, got.Info.Name, want)
+		t.Errorf("client.Package.Get(%q) = %s; want %s", pythonPackage, got.Info.Name, want)
 	}
 
 	want = "MIT license"
 	if got.Info.License != want {
-		t.Errorf("client.Package.Get(%q) = %s; want %s", arg, got.Info.License, want)
+		t.Errorf("client.Package.Get(%q) = %s; want %s", pythonPackage, got.Info.License, want)
 	}
 
 	want = "pytest-5.4.2-py3-none-any.whl"
 	if got.URLS[0].Filename != want {
-		t.Errorf("client.Package.Get(%q) = %s; want %s", arg, got.URLS[0].Filename, want)
+		t.Errorf("client.Package.Get(%q) = %s; want %s", pythonPackage, got.URLS[0].Filename, want)
 	}
 
 	want = "pytest-5.4.2.tar.gz"
 	if got.URLS[1].Filename != want {
-		t.Errorf("client.Package.Get(%q) = %s; want %s", arg, got.URLS[1].Filename, want)
+		t.Errorf("client.Package.Get(%q) = %s; want %s", pythonPackage, got.URLS[1].Filename, want)
 	}
 }
